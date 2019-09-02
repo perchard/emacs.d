b5a589d54ad961360891e07a22182e8804da576d
Sai Perchard <sai@perchard.com>
2019-09-01 21:20:38 -0700

    don't show empty groups in ibuffer

--- init.el
+++ init.el
@@ -100,6 +100,7 @@
     (ibuffer-vc-set-filter-groups-by-vc-root)
     (unless (eq ibuffer-sorting-mode 'alphabetic)
       (ibuffer-do-sort-by-alphabetic))))
+(setq-default ibuffer-show-empty-filter-groups nil)
 
 ;; save state between sessions
 (desktop-save-mode 1)
